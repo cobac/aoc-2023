c6a0bddffe8ea42f180e31f836ba2c7fe28f00d5
David Coba <coba@cobac.eu>
2023-12-19 19:28:00 +0100

    day1p2 failed attempt

--- src/day1.rs
+++ src/day1.rs
@@ -13,6 +13,29 @@ pub fn p1(input: &str) -> u32 {
         .collect();
     // println!("{v:?}");
 
+#[aoc(day1, part2, coba)]
+pub fn p2(input: &str) -> u32 {
+    // This approach doesn't work coz e.g. "eightwothree" needs to be "8wo3"
+    let v: Vec<_> = input
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9")
+        .lines()
+        .map(|line| {
+            line.chars()
+                .filter(|char| char.is_numeric())
+                .map(|x| x.to_digit(10).unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect();
+
+    // println!("{v:?}");
     v.into_iter()
         .map(|x| x.first().unwrap() * 10 + x.last().unwrap())
         .sum()
@@ -29,13 +52,17 @@ mod tests {
             a1b2c3d4e5f
             treb7uchet";
         assert_eq!(p1(input), 142);
-        assert_eq!(
-            p1("1
-2
-3
-4
-"),
-            11 + 22 + 33 + 44
-        )
+    }
+
+    #[test]
+    fn p2_test() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!(p2(input), 281);
     }
 }
