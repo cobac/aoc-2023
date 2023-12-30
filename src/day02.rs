a450ec01c8a654e3d8f45677feef72439b3d133d
David Coba <coba@cobac.eu>
2023-12-30 18:00:30 +0100

    stop pretending

--- src/day02.rs
+++ src/day02.rs
@@ -2,5 +2,7 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day2, part1, coba)]
 pub fn p1(_input: &str) -> u32 {
+    // yeah whatever split on : split on ; split on ,
+    // parse do the thing i really like programming but I don't think i can enjoy this
     3
 }
