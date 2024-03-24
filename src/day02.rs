cc50670ca4cf79cd950e294cffbc877f83802a8f
Coba <coba@cobac.eu>
2024-03-24 20:49:42 +0100

    day2p1

--- src/day02.rs
+++ src/day02.rs
@@ -16,12 +16,14 @@ enum Cubes {
 impl FromStr for Cubes {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (count, color) = s.split_once(" ").expect("color count splitting");
+        let (count, color) = s.split_once(' ').expect("color count splitting");
         match (count, color) {
             (count, "green") => Ok(Self::Green(count.parse().expect("should be a int"))),
             (count, "red") => Ok(Self::Red(count.parse().expect("should be a int"))),
             (count, "blue") => Ok(Self::Blue(count.parse().expect("should be a int"))),
-            _ => Err("Can't parse thing into cube counts".into()),
+            _ => Err(format!(
+                "Can't parse ({count:?}, {color:?}) into cube counts"
+            )),
         }
     }
 }
@@ -34,26 +36,32 @@ pub fn p1(input: &str) -> u32 {
     input
         .lines()
         .map(|line| {
-            line.split(": ")
-                .last()
-                .expect("No Game: str found")
+            let (game, sets) = line.split_once(": ").expect("no ':' found");
+            let invalid = sets
                 .split("; ")
                 .map(|set| {
-                    {
-                        set.split(", ").map(|cube| {
-                            match cube.parse::<Cubes>().expect("cube no parseable") {
+                    set.split(", ")
+                        .map(
+                            |cube| match cube.parse::<Cubes>().expect("cube no parseable") {
                                 Cubes::Green(count) => count > MAX_GREEN,
                                 Cubes::Red(count) => count > MAX_RED,
                                 Cubes::Blue(count) => count > MAX_BLUE,
-                            }
-                        })
-                    }
-                    .any(|x| x)
+                            },
+                        )
+                        .any(|x| x)
                 })
-                .collect::<Vec<_>>()
+                .any(|x| x);
+            (game, invalid)
         })
-        .collect::<Vec<_>>();
-    3
+        .filter(|(_, invalid)| !invalid)
+        .map(|(game, _)| {
+            game.split_once(' ')
+                .unwrap()
+                .1
+                .parse::<u32>()
+                .expect("no int found")
+        })
+        .sum()
 }
 
 #[cfg(test)]
